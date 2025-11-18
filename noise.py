2f4b213f865c8bf3fd7c6d77cddb8f3df3882589
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2025-11-18 11:05:13 +0200

    add noise.py

---
+++ noise.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+
+from file_handler import load_nifti, save_nifti
+
+MEAN = 100
+STD = 500
+
+
+def add_noise(data: np.ndarray) -> np.ndarray:
+    return data + np.random.normal(MEAN, STD, data.shape)
+
+
+if __name__ == '__main__':
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+
+    data, affine, header = load_nifti(input_path)
+    data = add_noise(data)
+    save_nifti(output_path, data, affine, header)
