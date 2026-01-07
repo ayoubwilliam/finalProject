8e2a84b611f46bc6d3778f7b6d5f5c56b11d7a63
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2026-01-07 03:53:10 +0200

    create lungs segmentation for ct files- with tmp files

--- seg_generator.py
+++ seg_generator.py
@@ -1,5 +1,6 @@
 from totalsegmentator.python_api import totalsegmentator
 import os
+import tempfile
 
 from file_handler import merge_nifti
 
@@ -34,23 +35,22 @@ def run_segmentation(input_path: str, output_path: str, task: str, subset: list,
 
 def create_path(folder: str, filename: str, scan_index: int, is_folder: bool = False) -> str:
     path = folder + filename + str(scan_index)
-    if is_folder:
-        return path
-    return path + FILE_EXTENSION
+    return path if is_folder else path + FILE_EXTENSION
 
 
 def create_lungs_seg() -> None:
+    os.makedirs(SEG_DIR, exist_ok=True)
+
     for scan_index in range(1, NUMBER_OF_CT_SCANS + 1):
         # create paths
         input_path = create_path(INPUT_DIR, CT_FILENAME, scan_index)
-        seg_path = create_path(SEG_DIR, SEG_FILENAME, scan_index, True)
         seg_filename = create_path(SEG_DIR, SEG_FILENAME, scan_index)
-        print(seg_path, seg_filename)
 
-        # run_segmentation(input_path, seg_path, SEG_TASK, ROI_SUBSET)
-
-        lobes = [os.path.join(seg_path, f) for f in os.listdir(seg_path)]
-        merge_nifti(seg_filename, *lobes)
+        # Temporary folder for this scan's lobe outputs
+        with tempfile.TemporaryDirectory(prefix=f"totseg_scan{scan_index}_") as tmp_dir:
+            run_segmentation(input_path, tmp_dir, SEG_TASK, ROI_SUBSET)
+            lobes = [os.path.join(tmp_dir, f) for f in os.listdir(tmp_dir)]
+            merge_nifti(seg_filename, *lobes)
 
 
 if __name__ == '__main__':
