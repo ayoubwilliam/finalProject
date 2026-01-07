cba67fe775fbb435e16d3934ba41e3a1e1482917
id-214193831 <agam.hershkodek@mail.huji.ac.il>
2026-01-07 07:06:42 +0200

    work with Itamar's data

--- project_paths.py
+++ project_paths.py
@@ -1,6 +1,21 @@
-# Paths
-INPUT_DIR = "./ct/"
-OUTPUT_DIR = "pipeline_output/"
-SEG_DIR = "./segmentations/"
+# # Paths (Itamar's files): CT-RATE_Scans folder
+# INPUT_DIR = "../itamar_sab/LongitudinalCXRAnalysis/CT_scans/CT-RATE_Scans"
+# OUTPUT_DIR = "./pipeline_output_itamar_files/scans/CT-RATE_Scans"
+# SEG_DIR = "./segmentations_itamar_files/CT-RATE_Scans"
+
+# Paths (Itamar's files): LUNA_scans folder
+INPUT_DIR = "../itamar_sab/LongitudinalCXRAnalysis/CT_scans/LUNA_scans"
+OUTPUT_DIR = "./pipeline_output_itamar_files/LUNA_scans/"
+SEG_DIR = "./segmentations_itamar_files/LUNA_scans"
+
+# Paths (Itamar's files): scans folder
+# INPUT_DIR = "../itamar_sab/LongitudinalCXRAnalysis/CT_scans/scans"
+# OUTPUT_DIR = "./pipeline_output_itamar_files/scans/"
+# SEG_DIR = "./segmentations_itamar_files/scans"
+
+# Paths (original 3 ct files)
+# INPUT_DIR = "./ct/"
+# OUTPUT_DIR = "pipeline_output/"
+# SEG_DIR = "./segmentations/"
 
 FILE_EXTENSION = ".nii.gz"
